1f3bf2c9893ccae6a80e981c199a4509416ce3d0
Anthony Pyrtle <xxpyrtlexx93@gmail.com>
2015-06-28 21:33:56 -0400

    Update device.mk

--- device.mk
+++ device.mk
@@ -16,6 +16,8 @@
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
+$(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-dalvik-heap.mk)
+
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/samsung/trltespr/trltespr-vendor.mk)
 
