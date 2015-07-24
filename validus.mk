28cac5175eb04ab7fa8118fbd076b36d0824fc26
Anthony Pyrtle <xxpyrtlexx93@gmail.com>
2015-07-24 11:29:17 -0400

    Vali-fy

--- validus.mk
+++ validus.mk
@@ -17,10 +17,10 @@
 $(call inherit-product, device/samsung/trltespr/full_trltespr.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/tesla/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/validus/config/nfc_enhanced.mk)
 
-# Inherit common Tesla phone.
-$(call inherit-product, vendor/tesla/config/common_full_phone.mk)
+# Inherit common Validus phone.
+$(call inherit-product, vendor/validus/config/common_full_phone.mk)
 
 PRODUCT_DEVICE := trltespr
-PRODUCT_NAME := tesla_trltespr
+PRODUCT_NAME := validus_trltespr
